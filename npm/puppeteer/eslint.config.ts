d035fa9f801a8303cd8c4e6840b50b3e2c30382e
Cacie Prins <cacie@cypress.io>
2025-01-15 09:20:47 -0500

    redundant rules in puppeteer

--- npm/puppeteer/eslint.config.ts
+++ npm/puppeteer/eslint.config.ts
@@ -1,10 +1,7 @@
 import { baseConfig } from '../../eslint.config'
-import globals from 'globals'
+
 export default [
   ...baseConfig,
-  {
-    ignores: ['**/dist'],
-  },
   {
     languageOptions: {
       globals: {
