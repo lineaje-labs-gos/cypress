4ffe59170241078fafc3254fba02bed5e5ff804b
Cacie Prins <cacie@cypress.io>
2025-03-19 13:32:12 -0400

    skip non chromium browsers for press() driver test

--- packages/driver/cypress/e2e/commands/actions/press.cy.ts
+++ packages/driver/cypress/e2e/commands/actions/press.cy.ts
@@ -1,5 +1,9 @@
 describe('__placeholder__/commands/actions/press', () => {
   it('dispatches the tab keypress to the AUT', () => {
+    if (Cypress.browser.family !== 'chromium') {
+      return
+    }
+
     cy.visit('/fixtures/input_events.html')
     cy.get('#focus').focus().then(() => {
       return Cypress.automation('key:press', { key: 'Tab' })
