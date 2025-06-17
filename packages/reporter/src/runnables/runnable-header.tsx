b818b4fcf617002aa12d71b5e78d08ff57facd59
Mabel Amaya <mabel@cypress.io>
2025-06-17 11:55:56 -0400

    add open in ide on header hover

--- packages/reporter/src/runnables/runnable-header.tsx
+++ packages/reporter/src/runnables/runnable-header.tsx
@@ -3,9 +3,11 @@ import React, { ReactElement } from 'react'
 
 import type { StatsStore } from '../header/stats-store'
 import { formatDuration, getFilenameParts } from '../lib/util'
-import FileNameOpener from '../lib/file-name-opener'
 import { RunnablesStore } from './runnables-store'
 import { DebugDismiss } from '../header/DebugDismiss'
+import Button from '@cypress-design/react-button'
+import events from '../lib/events'
+import { IconWindowCodeEditor } from '@cypress-design/react-icon'
 
 const renderRunnableHeader = (children: ReactElement) => <div className="runnable-header" data-cy="runnable-header">{children}</div>
 
@@ -49,9 +51,16 @@ const RunnableHeader: React.FC<RunnableHeaderProps> = observer(({ spec, statsSto
     relativeFile: relativeSpecPath,
   }
 
+  const openInIDE = () => {
+    return <Button size='20' variant='outline-dark' className='open-in-ide-button' onClick={() => events.emit('open:file:unified', fileDetails)}><IconWindowCodeEditor strokeColor='gray-500' fillColor='gray-900' /> Open in IDE </Button>
+  }
+
   return renderRunnableHeader(
     <>
-      <FileNameOpener fileDetails={fileDetails} />
+      <div className='runnable-header-file-name'>
+        {fileDetails.displayFile || fileDetails.originalFile}{!!fileDetails.line && `:${fileDetails.line}`}{!!fileDetails.column && `:${fileDetails.column}`}
+        {openInIDE()}
+      </div>
       {runnablesStore.testFilter && runnablesStore.totalTests > 0 && <DebugDismiss matched={runnablesStore.totalTests} total={runnablesStore.totalUnfilteredTests} />}
       {Boolean(statsStore.duration) && (
         <span className='duration' data-cy="spec-duration">{formatDuration(statsStore.duration)}</span>
