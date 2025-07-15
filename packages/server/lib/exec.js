1e6cb9c390c766e13b09532189123d721f325e5c
Jennifer Shehane <shehane.jennifer@gmail.com>
2025-07-15 13:55:25 -0400

    bump execa versions to 4.1.0

--- packages/server/lib/exec.js
+++ packages/server/lib/exec.js
@@ -32,7 +32,7 @@ module.exports = {
       log('and is running command:', options.cmd)
       log('in folder:', projectRoot)
 
-      return execa.shell(cmd, { cwd, env, shell })
+      return execa(cmd, { cwd, env, shell })
       .then((result) => {
         // do we want to return all fields returned by execa?
         result.shell = shell
@@ -40,7 +40,12 @@ module.exports = {
 
         return result
       }).then(pickMainProps)
-      .catch(pickMainProps) // transform rejection into an object
+      .catch((result) => {
+        result.code = result.exitCode
+
+        return result
+      })
+      .then(pickMainProps) // transform rejection into an object
       .then(trimStdio)
     }
 
